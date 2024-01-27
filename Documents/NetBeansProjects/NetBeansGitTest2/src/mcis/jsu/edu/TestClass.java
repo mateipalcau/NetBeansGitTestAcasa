b09a9eda432efb3d0cfa104ba07c851da2f3b9aa
Matei Palcau <mpalcau@stu.jsu.edu>
2024-01-27 12:35:13 -0600

    Initial Commit

---
+++ Documents/NetBeansProjects/NetBeansGitTest2/src/mcis/jsu/edu/TestClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mcis.jsu.edu;
+
+/**
+ *
+ * @author mateirares
+ */
+public class TestClass {
+    public static String testMethod1(){
+        return "Hello World";
+    }
+}
