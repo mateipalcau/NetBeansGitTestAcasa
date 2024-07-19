76d24ca63be68e8f2be9f2ee6cef06b3ac248a4c
Matei Palcau <mpalcau@stu.jsu.edu>
2024-07-19 15:50:53 +0300

    Commit3

--- Desktop/Gittt/TestGit1/src/mcis/jsu/edu/Main.java
+++ Desktop/Gittt/TestGit1/src/mcis/jsu/edu/Main.java
@@ -11,5 +11,6 @@ package mcis.jsu.edu;
 public class Main {
     public static void main(String[] args) {
        System.out.println(TestClass.testMethod1());
+       System.out.println("Buna");
     }
 }
