9a04ede5ddf423ac87057cce0db35096c2be658e
Matei Palcau <mpalcau@stu.jsu.edu>
2024-07-22 15:26:08 +0300

    Commit2

---
+++ Desktop/NetBeansGitTestAcasa/NetBeansGitTestAcasa/test/mcis/jsu/edu/JUnitTest.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package mcis.jsu.edu;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mateirares
+ */
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testClassMethod1() {
+       assertEquals(TestClass.testMethod1(), "Hello, World!");
+    }
+    
+    @Test
+    public void testClassMethod2() {
+       assertEquals(TestClass.testMethod2(), "Hello there, world!");
+    }
+}
